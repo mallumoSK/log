03e0025bbc8391a8f9773bc5b7280dd47c9454a9
mallumo <develop.mallumo@gmail.com>
2024-03-28 13:09:04 +0100

    1.9.23-1.0.1 - kotlin 1.9.23

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -2,7 +2,7 @@
 object Deps {
     object lib {
         const val group = "tk.mallumo"
-        const val version = "1.9.10-1.0.0"
+        const val version = "1.9.23-1.0.1"
         const val artifact = "log"
     }
 
